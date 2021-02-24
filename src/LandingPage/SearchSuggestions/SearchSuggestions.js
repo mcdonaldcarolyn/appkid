b0e14eee7e31138ef5793f0153387484209071cd
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2021-02-24 13:35:10 -0500

    add search suggestion

--- src/LandingPage/SearchSuggestions/SearchSuggestions.js
+++ src/LandingPage/SearchSuggestions/SearchSuggestions.js
@@ -4,11 +4,30 @@ import styles from './SearchSuggestions.module.css';
 export function SearchSuggestions (){
   return(
     <div className={styles.suggestions}>
-      <span className={`icon is-small`}>
-            <i className="fas fa-search"></i>
-          </span>
-      Search suggestions
+      <span className='icon is-small'>
+            <i className="fas fa-trees"></i>
+      </span>
+      <span className={styles.suggestion}>
+        Parks
+      </span>
+      <span className='icon is-small'>
+            <i className="fas fa-baby-carriage"></i>
+      </span>
+      <span className={styles.suggestion}>
+        Stroller friendly
+      </span>
+      <span className='icon is-small'>
+            <i className="fas fa-landmark"></i>
+      </span>
+      <span className={styles.suggestion}>
+        Playgrounds
+      </span>
+      <span className='icon is-small'>
+            <i className="fas fa-utensils"></i>
+      </span>
+      <span className={styles.suggestion}>
+      kid friendly restaurants
+      </span>
     </div>
-
   )
 }
