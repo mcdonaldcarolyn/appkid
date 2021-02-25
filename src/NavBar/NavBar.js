3310d363d2896f7cc8eb6e4410615e23a9135832
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2021-02-25 09:53:03 -0500

    added nav bar elements

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../assets/logo.png'
+import styles from './NavBar.module.css';
+import { SearchBar } from '../LandingPage/SearchBar/SearchBar';
+
+export function NavBar() {
+  return (
+    <div className={styles['nav-bar']}>
+      <img src={logo} className={styles.logo} alt='kidtivity logo' />
+      <SearchBar />
+      <button className={`button ${styles}`}> Sign In</button>
+      <button className={`button ${styles}`}> Register</button>
+    </div>
+  );
+}
