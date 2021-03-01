0b63a160fefb6d07436d238848c6a91a620c4486
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2021-03-01 10:23:47 -0500

    add to search result

--- src/Search/SearchResult/SearchResult.js
+++ src/Search/SearchResult/SearchResult.js
@@ -3,9 +3,18 @@ import styles from './SearchResult.module.css'
 export function SearchResult() {
   return (
     <div className={styles['search-result']}>
-      <img src="https://via.placeholder.com/150C"  alt="buisiness image"/>
-      <p>General Info</p>
-      <p>Address</p>
+      <img src="https://via.placeholder.com/150C" alt="buisiness image" className={styles['business-image']}/>
+      <div className={styles['business-info']}>
+        <h2 className="subtitle">Fun kid thing</h2>
+        <p>Rating</p>
+        <p>$$<span className="tag">All ages</span> <span className="tag">Outdoor Farm activity</span></p>
+      </div>
+      <div>
+        <p>phone number</p>
+        <p>street address</p>
+        <p>town, zip</p>
+      </div>
+      
     </div>
   )
 }
