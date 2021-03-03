5d2437a499663d264a5156ae3445a072c9fc6077
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2021-03-03 09:19:07 -0500

    add router and encodedURL

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from '../assets/logo.png'
 import styles from './NavBar.module.css';
 import { SearchBar } from '../LandingPage/SearchBar/SearchBar';
+import { Link } from 'react-router-dom';
 
 export function NavBar() {
   return (
     <div className={styles['nav-bar']}>
-      <img src={logo} className={styles.logo} alt='kidtivity logo' />
+      <Link to='/'> <img src={logo} className={styles.logo} alt='kidtivity logo' /></Link>
       <SearchBar small />
       <button className={`button ${styles}`}> Sign In</button>
       <button className={`button ${styles}`}> Register</button>
