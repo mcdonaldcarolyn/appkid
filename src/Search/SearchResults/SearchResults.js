5029ec776f5da3ea8ab4a112d6da163976fd71f2
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2021-03-24 09:43:27 -0400

    add spinner

--- src/Search/SearchResults/SearchResults.js
+++ src/Search/SearchResults/SearchResults.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { SearchResult } from '../SearchResult/SearchResult';
 import styles from "./SearchResults.module.css";
+import { Spinner } from '../Spinner/Spinner';
 
 
 export function SearchResults(props) {
-  console.log("blue");
-  console.log(props.businesses);
-  if(!props.businesses || !props.businesses.length){
-    return (<div>error</div>);
+  let searchResults = <Spinner />;
+  if (props.businesses && props.businesses.length) {
+    
+    searchResults = props.businesses.map(b => <SearchResult key={b.id} businesses={b}/>)
   }
 
-  const searchResults = props.businesses.map(b => <SearchResult key={b.id} businesses={b}/>)
+  
   
   return (
     <div className={styles['search-results']}>
